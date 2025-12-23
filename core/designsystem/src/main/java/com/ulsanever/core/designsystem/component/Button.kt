55f1bdc5b6c9c31abe57323394bf8f9007dd850f
jaemin-Yoo <iamjm29@gmail.com>
2025-12-23 20:32:41 +0900

    Feat: 커스텀 버튼 디자인 수정 및 테두리 버튼 구현
    - trailing icon 추가
    - Outlined Button 구현

--- core/designsystem/src/main/java/com/ulsanever/core/designsystem/component/Button.kt
+++ core/designsystem/src/main/java/com/ulsanever/core/designsystem/component/Button.kt
@@ -2,40 +2,46 @@ package com.ulsanever.core.designsystem.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ulsanever.core.designsystem.theme.LangChatTheme
 import com.ulsanever.core.designsystem.theme.ThemePreviews
 
 /**
- * LangChat 버튼. Material 3 [Button] 래핑
+ * LangChat 버튼
  *
+ * @param text 버튼 텍스트 내용
  * @param onClick 버튼 클릭 시 호출되는 콜백
  * @param modifier 버튼 modifier
  * @param enabled 버튼 활성화 여부
- * @param contentPadding 버튼 container와 content 사이 padding
- * @param content 버튼 content
+ * @param colors 버튼 색상
+ * @param leadingIcon 버튼 왼쪽 아이콘. 'null' 이면 아이콘 없음
+ * @param trailingIcon 버튼 오른쪽 아이콘. 'null' 이면 아이콘 없음
  */
 @Composable
 fun LangChatButton(
+    text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    colors: ButtonColors = ButtonDefaults.buttonColors(
-        containerColor = MaterialTheme.colorScheme.onBackground,
-    ),
-    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-    content: @Composable RowScope.() -> Unit,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    leadingIcon: ImageVector? = null,
+    trailingIcon: ImageVector? = null,
 ) {
     Button(
         onClick = onClick,
@@ -43,52 +49,87 @@ fun LangChatButton(
         enabled = enabled,
         shape = RoundedCornerShape(8.dp),
         colors = colors,
-        contentPadding = contentPadding,
-        content = content,
-    )
-}
-
-/**
- * LangChat 텍스트, 아이콘 버튼
- *
- * @param onClick 버튼 클릭 시 호출되는 콜백
- * @param text 버튼 텍스트 내용
- * @param modifier 버튼 modifier
- * @param enabled 버튼 활성화 여부
- * @param leadingIcon 버튼 아이콘. 'null' 이면 아이콘 없음
- */
-@Composable
-fun LangChatButton(
-    onClick: () -> Unit,
-    text: String,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    colors: ButtonColors = ButtonDefaults.buttonColors(
-        containerColor = MaterialTheme.colorScheme.onBackground,
-    ),
-    leadingIcon: @Composable (() -> Unit)? = null,
-) {
-    LangChatButton(
-        onClick = onClick,
-        modifier = modifier,
-        enabled = enabled,
-        colors = colors,
-        contentPadding = PaddingValues(
-            start = 16.dp,
-            top = 16.dp,
-            end = 16.dp,
-            bottom = 16.dp
-        )
+        contentPadding = PaddingValues(16.dp)
     ) {
         if (leadingIcon != null) {
             Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
-                leadingIcon()
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = null,
+                )
             }
         }
         Box(
-            Modifier.padding(start = if (leadingIcon != null) ButtonDefaults.IconSpacing else 0.dp),
+            modifier = Modifier.padding(
+                start = if (leadingIcon != null) ButtonDefaults.IconSpacing else 0.dp,
+                end = if (trailingIcon != null) ButtonDefaults.IconSpacing else 0.dp,
+            ),
         ) {
             Text(text = text)
+        }
+        if (trailingIcon != null) {
+            Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
+                Icon(
+                    imageVector = trailingIcon,
+                    contentDescription = null,
+                )
+            }
+        }
+    }
+}
+
+/**
+ * LangChat 테두리 버튼
+ *
+ * @param text 버튼 텍스트 내용
+ * @param onClick 버튼 클릭 시 호출되는 콜백
+ * @param modifier 버튼 modifier
+ * @param enabled 버튼 활성화 여부
+ * @param colors 버튼 색상
+ * @param leadingIcon 버튼 왼쪽 아이콘. 'null' 이면 아이콘 없음
+ * @param trailingIcon 버튼 오른쪽 아이콘. 'null' 이면 아이콘 없음
+ */
+@Composable
+fun LangChatOutlinedButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.outlinedButtonColors(),
+    leadingIcon: ImageVector? = null,
+    trailingIcon: ImageVector? = null,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = RoundedCornerShape(8.dp),
+        colors = colors,
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        if (leadingIcon != null) {
+            Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = null,
+                )
+            }
+        }
+        Box(
+            modifier = Modifier.padding(
+                start = if (leadingIcon != null) ButtonDefaults.IconSpacing else 0.dp,
+                end = if (trailingIcon != null) ButtonDefaults.IconSpacing else 0.dp,
+            ),
+        ) {
+            Text(text = text)
+        }
+        if (trailingIcon != null) {
+            Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
+                Icon(
+                    imageVector = trailingIcon,
+                    contentDescription = null,
+                )
+            }
         }
     }
 }
@@ -97,6 +138,44 @@ fun LangChatButton(
 @Composable
 private fun LangChatButtonPreview() {
     LangChatTheme {
-        LangChatButton(onClick = {}, text = "테스트 버튼")
+        LangChatButton(
+            text = "테스트 버튼",
+            onClick = {},
+        )
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun LangChatOutlinedButtonPreview() {
+    LangChatTheme {
+        LangChatOutlinedButton(
+            text = "테스트 버튼",
+            onClick = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LangChatLeadingIconButtonPreview() {
+    LangChatTheme {
+        LangChatButton(
+            text = "테스트 버튼",
+            onClick = {},
+            leadingIcon = Icons.Default.Add,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LangChatTrailingIconButtonPreview() {
+    LangChatTheme {
+        LangChatButton(
+            onClick = {},
+            text = "테스트 버튼",
+            trailingIcon = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+        )
     }
 }
